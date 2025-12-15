fe76745696cc478af77d7477f3c0182d168cbe60
fResult <styxmaz@gmail.com>
2025-12-16 00:43:40 +0700

    Implement arithmetic operations and validation for Money value object.

--- src/main/kotlin/com/fResult/resilienceWalletLedger/wallet/internal/domain/model/Money.kt
+++ src/main/kotlin/com/fResult/resilienceWalletLedger/wallet/internal/domain/model/Money.kt
@@ -2,4 +2,24 @@ package com.fResult.resilienceWalletLedger.wallet.internal.domain.model
 
 import java.math.BigDecimal
 
-data class Money(val amount: BigDecimal, val currency: Currency)
+data class Money(val amount: BigDecimal, val currency: Currency) {
+  companion object {
+    fun zero(currency: Currency) = Money(BigDecimal.ZERO, currency)
+  }
+
+  operator fun plus(other: Money): Money {
+    require(this.currency == other.currency) {
+      "Currency mismatch! Cannot add ${other.currency} to ${this.currency}"
+    }
+
+    return Money(this.amount.add(other.amount), this.currency)
+  }
+
+  operator fun minus(other: Money): Money {
+    require(this.currency == other.currency) {
+      "Currency mismatch! Cannot subtract ${other.currency} from ${this.currency}"
+    }
+
+    return Money(this.amount.subtract(other.amount), this.currency)
+  }
+}
