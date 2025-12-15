ff965c08e5db1b35ef0dd2b61498758dcbc97ba0
fResult <styxmaz@gmail.com>
2025-12-16 00:38:43 +0700

    Model `WalletStatus`.

--- src/main/kotlin/com/fResult/resilienceWalletLedger/wallet/internal/domain/model/WalletStatus.kt
+++ src/main/kotlin/com/fResult/resilienceWalletLedger/wallet/internal/domain/model/WalletStatus.kt
@@ -1,3 +1,7 @@
 package com.fResult.resilienceWalletLedger.wallet.internal.domain.model
 
-enum class WalletStatus
+enum class WalletStatus(val value: String) {
+  ACTIVE("Active"),
+  INACTIVE("Inactive"),
+  SUSPENDED("Suspended"),
+}
