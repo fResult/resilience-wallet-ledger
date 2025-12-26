6943e5d5b7bd96b8ca3e83b3a5fd9dd8dcd2eae2
fResult <styxmaz@gmail.com>
2025-12-26 22:30:09 +0700

    feat(wallet): draft create wallet signature for TDD
    - Define `createWallet` method stub in `WalletService` to establish the API contract
    - Defer implementation with `TODO` to facilitate the Red-Green-Refactor cycle
    - Import necessary domain and reactive types to support upcoming unit test cases

--- src/main/kotlin/com/fResult/resilienceWalletLedger/wallet/internal/application/service/WalletService.kt
+++ src/main/kotlin/com/fResult/resilienceWalletLedger/wallet/internal/application/service/WalletService.kt
@@ -1,7 +1,20 @@
 package com.fResult.resilienceWalletLedger.wallet.internal.application.service
 
 import com.fResult.resilienceWalletLedger.wallet.internal.application.port.out.WalletRepository
+import com.fResult.resilienceWalletLedger.wallet.internal.domain.exception.WalletException
+import com.fResult.resilienceWalletLedger.wallet.internal.domain.model.Currency
+import com.fResult.resilienceWalletLedger.wallet.internal.domain.model.OwnerId
+import com.fResult.resilienceWalletLedger.wallet.internal.domain.model.Wallet
+import io.vavr.control.Either
+import reactor.core.publisher.Mono
 
 class WalletService(
   walletRepository: WalletRepository,
-)
+) {
+  fun createWallet(
+    ownerId: OwnerId,
+    currency: Currency,
+  ): Mono<Either<WalletException, Wallet>> {
+    TODO("Not yet implemented")
+  }
+}
