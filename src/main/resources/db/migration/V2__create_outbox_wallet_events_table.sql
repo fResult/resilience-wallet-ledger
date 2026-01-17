dd932a9b71a9d14c92a7962d4a2e29f246eee87c
fResult <styxmaz@gmail.com>
2026-01-18 03:10:11 +0700

    feat(persistence): refine outbox schema with tracing and versioning
    - Change `aggregate_id` to UUID and add indix for performance
    - Add `aggregate_version` to support event ordering and optimistic locking
    - Include `trace_id` and `span_id`for distributed tracing observability

--- src/main/resources/db/migration/V2__create_outbox_wallet_events_table.sql
+++ src/main/resources/db/migration/V2__create_outbox_wallet_events_table.sql
@@ -1,12 +1,21 @@
 CREATE TABLE outbox_wallet_events (
-  id UUID PRIMARY KEY,
-  aggregate_type VARCHAR(50) NOT NULL,
-  aggregate_id VARCHAR(36) NOT NULL,
-  event_type VARCHAR(100) NOT NULL,
-  payload JSONB NOT NULL,
-  occurred_on TIMESTAMP WITH TIME ZONE NOT NULL,
-  created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW()
+  id UUID PRIMARY KEY,                 -- UUIDv7 (Time-Sorted)
+  aggregate_type VARCHAR(50) NOT NULL, -- "WALLET" (For Change Data Capture (CDC) Tools)
+  aggregate_id UUID NOT NULL,          -- Wallet ID
+  aggregate_version INTEGER NOT NULL,
+
+  trace_id VARCHAR(64) NOT NULL UNIQUE,
+  span_id VARCHAR(64) NOT NULL UNIQUE,
+
+  event_type VARCHAR(100) NOT NULL,    -- "WalletCreated", "MoneyDeposited", "WalletCreated"
+  payload JSONB NOT NULL,              -- Event Data
+  occurred_on TIMESTAMP WITH TIME ZONE NOT NULL,                         -- Domain Time
+  created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP -- System Time
 );
+
+-- Index For Query Wallet (Time-Sorted)
+CREATE INDEX idx_outbox_wallet_aggregate_id
+ON outbox_wallet_events(aggregate_id);
 
 CREATE INDEX idx_outbox_wallet_created_at
 ON outbox_wallet_events(created_at);
