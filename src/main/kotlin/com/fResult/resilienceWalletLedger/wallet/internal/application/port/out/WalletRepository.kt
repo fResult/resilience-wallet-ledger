b1f8ffddea3fd788d083553d77d3f40110fffb3c
fResult <styxmaz@gmail.com>
2026-01-20 20:13:03 +0700

    refactor(wallet): update repository signature to support atomic event persistence
    - Modify `save` to accept `Pair<Wallet List<WalletEvent>>` instead of just `Wallet`
    - Enable Outbox pattern by enforcing simultaneous persistance of state and events
    - Align with the "Read-Decide-Write" consistency pattern

--- src/main/kotlin/com/fResult/resilienceWalletLedger/wallet/internal/application/port/out/WalletRepository.kt
+++ src/main/kotlin/com/fResult/resilienceWalletLedger/wallet/internal/application/port/out/WalletRepository.kt
@@ -1,5 +1,6 @@
 package com.fResult.resilienceWalletLedger.wallet.internal.application.port.out
 
+import com.fResult.resilienceWalletLedger.wallet.internal.domain.event.WalletEvent
 import com.fResult.resilienceWalletLedger.wallet.internal.domain.exception.WalletException
 import com.fResult.resilienceWalletLedger.wallet.internal.domain.model.Wallet
 import com.fResult.resilienceWalletLedger.wallet.internal.domain.model.WalletId
@@ -9,5 +10,7 @@ import reactor.core.publisher.Mono
 interface WalletRepository {
   fun findById(id: WalletId): Mono<Either<WalletException, Wallet>>
 
-  fun save(wallet: Wallet): Mono<Either<WalletException, Wallet>>
+  fun save(
+    wallet: Pair<Wallet, List<WalletEvent>>,
+  ): Mono<Either<WalletException, Pair<Wallet, List<WalletEvent>>>>
 }
