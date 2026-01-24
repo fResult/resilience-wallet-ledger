ed0c1383642c63be7d1af6f35c4ce063dad2175b
fResult <styxmaz@gmail.com>
2026-01-24 23:16:59 +0700

    refactor(common): explicate return type for bimapPair
    - Add explicit return type `Mono<Pair<C, D>>` for better API clarity and type inference
    - Include missing `reactor.core.publisher.Mono` import to ensure compilation

--- src/main/kotlin/com/fResult/resilienceWalletLedger/common/extension/MonoExtensions.kt
+++ src/main/kotlin/com/fResult/resilienceWalletLedger/common/extension/MonoExtensions.kt
@@ -1,6 +1,9 @@
 package com.fResult.resilienceWalletLedger.common.extension
 
+import io.vavr.control.Either
+import reactor.core.publisher.Mono
+
 fun <A, B, C, D> Mono<Pair<A, B>>.bimapPair(
   mapFirst: (A) -> C,
   mapSecond: (B) -> D,
-) = this.map { it.bimap(mapFirst, mapSecond) }
+): Mono<Pair<C, D>> = this.map { it.bimap(mapFirst, mapSecond) }
